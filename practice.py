6e900f871c9f7498053af13cf5f3aa03202d5461
Aniketsingh0302 <anikets32002@gmail.com>
2025-02-05 21:46:34 +0530

    added

---
+++ practice.py
@@ -0,0 +1,10 @@
+country_code= {
+     "INDIA": "001",
+    "USA": "002",
+    "UK": "003",
+    "AFGANISTAN":"004",
+}
+countryName = input("Enter the country name :")
+print(country_code.get(countryName,"Not present in the dictionary"))
+   
+    
