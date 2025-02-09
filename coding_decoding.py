6e0f4383690320a328fa3da384c2aee7873485ad
Aniketsingh0302 <anikets32002@gmail.com>
2025-02-09 18:06:34 +0530

    coded

---
+++ coding_decoding.py
@@ -0,0 +1,26 @@
+import string
+
+def reverse(message):
+     return message[::-1]
+choice = int(input("Enter 1 to code or 2 to decode \n"))
+coded =""
+if choice == 1:
+    msg = input("enter your message: \n")
+    if len(msg) <=3:
+        coded = reverse(msg)
+    else:
+         replaced_code =  msg.replace("a","@#")
+         coded =reverse(replaced_code)
+
+    print(coded)
+
+elif choice == 2:
+     coded=input("enter your message:")
+     if len(coded) <=3:
+          print(reverse(coded))
+     else:
+         replace_code = coded.replace("#@","a")
+         decode = reverse(replace_code)
+         print(decode)
+else:
+     print("Exiting...")
