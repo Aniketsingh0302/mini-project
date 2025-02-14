c62b492ec922eb9b28dca077b34d1b7cd8caf378
Aniketsingh0302 <anikets32002@gmail.com>
2025-02-14 21:29:26 +0530

    updated

--- librarymanagement.py
+++ librarymanagement.py
@@ -12,9 +12,14 @@ class library:
             print(f"self {self.numberofbooks}: {self.books[i]}")
     def comparison(self):
         if self.numberofbooks==len(self.books):
-            print("The library is updated and full")
+            print("The library is updated")
+            
         else:
             print("Some mismanagement in the library")
+        if self.numberofbooks >= 20:
+            print("The library is full")
+            
+           
         
 books=input("enter the list of books:").split()
 library=library(0,books)
