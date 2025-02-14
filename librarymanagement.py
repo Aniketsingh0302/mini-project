97525a565c7834260aa1eb7e1dc779b222f23904
Aniketsingh0302 <anikets32002@gmail.com>
2025-02-14 21:02:02 +0530

    library management

---
+++ librarymanagement.py
@@ -0,0 +1,23 @@
+
+
+class library:
+    def __init__(self,numberofbooks,books):
+        self.numberofbooks = numberofbooks
+        self.books=books
+
+    def record(self):
+        print("Record of books in the library:")
+        for i in range(len(self.books)):
+            self.numberofbooks+=1
+            print(f"self {self.numberofbooks}: {self.books[i]}")
+    def comparison(self):
+        if self.numberofbooks==len(self.books):
+            print("The library is updated and full")
+        else:
+            print("Some mismanagement in the library")
+        
+books=input("enter the list of books:").split()
+library=library(0,books)
+library.record()
+library.comparison()
+
